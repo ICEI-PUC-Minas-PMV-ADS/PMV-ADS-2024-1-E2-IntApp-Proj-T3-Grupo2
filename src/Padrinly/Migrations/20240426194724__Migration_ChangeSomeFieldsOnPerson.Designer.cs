8db524db919c7c7d592c71f353c576c370d25416
Felipe van Oorshot <66143166+Fena-lander@users.noreply.github.com>
2024-04-26 16:51:15 -0300

    Migration change some fields type

--- src/Padrinly/Migrations/20240426194724__Migration_ChangeSomeFieldsOnPerson.Designer.cs
+++ src/Padrinly/Migrations/20240426194724__Migration_ChangeSomeFieldsOnPerson.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Padrinly.Data;
@@ -11,9 +12,11 @@ using Padrinly.Data;
 namespace Padrinly.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240426194724__Migration_ChangeSomeFieldsOnPerson")]
+    partial class _Migration_ChangeSomeFieldsOnPerson
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Padrinly/Migrations/20240426194724__Migration_ChangeSomeFieldsOnPerson.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Padrinly.Migrations
+{
+    /// <inheritdoc />
+    public partial class _Migration_ChangeSomeFieldsOnPerson : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Persons",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "PhoneNumber",
+                table: "Persons",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
