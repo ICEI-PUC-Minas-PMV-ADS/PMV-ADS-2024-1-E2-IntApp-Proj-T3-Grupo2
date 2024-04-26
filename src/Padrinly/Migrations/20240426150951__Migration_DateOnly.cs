b1cd15a9035ccec9346664bbc827b2b40d70f75c
Felipe van Oorshot <66143166+Fena-lander@users.noreply.github.com>
2024-04-26 12:11:51 -0300

    Migration changed DateTime to DateOnly

---
+++ src/Padrinly/Migrations/20240426150951__Migration_DateOnly.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Padrinly.Migrations
+{
+    /// <inheritdoc />
+    public partial class _Migration_DateOnly : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "BirthDate",
+                table: "Persons",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "BirthDate",
+                table: "Persons",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+        }
+    }
+}
