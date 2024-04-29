ef6a75f44934c827aeff7da8dd18943b73e9433a
Felipe van Oorshot <66143166+Fena-lander@users.noreply.github.com>
2024-04-29 18:00:46 -0300

    Change type of fields on Person

---
+++ src/Padrinly/Migrations/20240429204540__Migration_FixedFieldsOnPerson.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Padrinly.Migrations
+{
+    /// <inheritdoc />
+    public partial class _Migration_FixedFieldsOnPerson : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "StudentResponsibleViewModel",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "StudentEmail",
+                table: "StudentResponsibleViewModel",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SecondDocument",
+                table: "Persons",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstDocument",
+                table: "Persons",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "StudentResponsibleViewModel");
+
+            migrationBuilder.DropColumn(
+                name: "StudentEmail",
+                table: "StudentResponsibleViewModel");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SecondDocument",
+                table: "Persons",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "FirstDocument",
+                table: "Persons",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
