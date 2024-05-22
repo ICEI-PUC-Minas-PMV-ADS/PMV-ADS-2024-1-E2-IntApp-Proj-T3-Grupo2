09260b4aff1f79b4bb1d53caaa5732529a0ffd83
Lidia Almeida <144849420+lidia1990@users.noreply.github.com>
2024-05-22 01:22:01 +0100

    WIP: Added extra fields

--- src/Padrinly/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ src/Padrinly/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -81,6 +81,25 @@ namespace Padrinly.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
+            [Required]
+            public string Name { get; set; }
+            /// <summary>
+            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            ///     directly from your code. This API may change or be removed in future releases.
+            /// </summary>
+            [Required]
+            public string Cpf { get; set; }
+            /// <summary>
+            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            ///     directly from your code. This API may change or be removed in future releases.
+            /// </summary>
+            [Required]
+            public string Phone { get; set; }
+
+            /// <summary>
+            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            ///     directly from your code. This API may change or be removed in future releases.
+            /// </summary>
             [Display(Name = "Remember me?")]
             public bool RememberMe { get; set; }
         }
