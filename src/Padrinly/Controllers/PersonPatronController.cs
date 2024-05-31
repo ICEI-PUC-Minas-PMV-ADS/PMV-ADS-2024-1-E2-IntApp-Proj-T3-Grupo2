bb83574a751018339c67bc97bed80954074d7d76
Felipe van Oorshot <66143166+Fena-lander@users.noreply.github.com>
2024-05-31 10:17:15 -0300

    Created method to affiliated student

---
+++ src/Padrinly/Controllers/PersonPatronController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Padrinly.Common.Extensions;
+using Padrinly.Data;
+using Padrinly.Domain.Entities;
+using Padrinly.Domain.Enums;
+
+namespace Padrinly.Controllers
+{
+    public class PersonPatronController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<User> _userManager;
+
+        public PersonPatronController(ApplicationDbContext context, UserManager<User> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Affiliate()
+        {
+            var students = await _context.Persons
+                .Where(s => s.Type == TypePerson.Student)
+                .ToListAsync();
+
+            var rnd = new Random();
+            var randomStudent = students[rnd.Next(students.Count)];
+
+            var userId = User.GetUserId();
+
+            var patron = await _context.Persons
+                .FirstOrDefaultAsync(p => p.IdUser == userId);
+
+            var patronList = await _context.PersonPatrons
+                .Where(pp => pp.IdPatron == patron.Id)
+                .ToListAsync();
+
+            while (patronList.Any(pp => pp.IdStudent == randomStudent.Id))
+            {
+                randomStudent = students[rnd.Next(students.Count)];
+            }
+            var personPatron = new PersonPatron
+            {
+                IdPatron = patron.Id,
+                IdStudent = randomStudent.Id,
+            };
+
+            _context.Add(personPatron);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
